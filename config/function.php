9e4c70d080339137b0075a71fa575bff1d9eef98
Ahmad Syarwani <ala52798@gmail.com>
2024-09-03 12:11:32 +0800

    update config

--- config/function.php
+++ config/function.php
@@ -3,4 +3,10 @@
         $hasil = 'Rp ' . number_format($angka, 2, ",", ".");
         return $hasil;
     }
+
+    function checkAccess($requiredRole) {
+        if(!isset($_SESSION['role_id']) || $_SESSION['role_id' != $requiredRole]) {
+            header('Location: index.php?page=login');
+        }
+    }
 ?>
