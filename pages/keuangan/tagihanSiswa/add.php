461f7d8f409733a5b0131fff02acd517176ab38d
Ahmad Syarwani <ala52798@gmail.com>
2025-01-07 10:30:42 +0800

    Perbaikan script tambah tagihan siswa

--- pages/keuangan/tagihanSiswa/add.php
+++ pages/keuangan/tagihanSiswa/add.php
@@ -6,25 +6,47 @@ if (isset($_POST['button_create'])) {
 
     // Array untuk menyimpan data tagihan
     $tagihanData = [
-        ['tarif_pembayaran_id' => 1, 'jumlah_tagihan' => $_POST['jumlah_tagihan1']],
-        ['tarif_pembayaran_id' => 2, 'jumlah_tagihan' => $_POST['jumlah_tagihan2']],
-        ['tarif_pembayaran_id' => 3, 'jumlah_tagihan' => $_POST['jumlah_tagihan3']],
+        ['jenis_pembayaran_id' => 1, 'jumlah_tagihan' => $_POST['jumlah_tagihan1']],
+        ['jenis_pembayaran_id' => 2, 'jumlah_tagihan' => $_POST['jumlah_tagihan2']],
+        ['jenis_pembayaran_id' => 3, 'jumlah_tagihan' => $_POST['jumlah_tagihan3']],
     ];
 
-    // Prepare the insert statement
+    // Ambil tarif_pembayaran_id berdasarkan tipe dan jenis_pembayaran_id
+    $tarifQuery = "
+        SELECT id AS tarif_pembayaran_id, jenis_pembayaran_id 
+        FROM tarif_pembayaran 
+        WHERE tipe = :tipe AND tahun_ajaran_id = :tahun_ajaran_id AND jenis_pembayaran_id = :jenis_pembayaran_id
+    ";
+    $tarifStmt = $db->prepare($tarifQuery);
+
+    // Query untuk insert data
     $insertSql = "INSERT INTO tagihan_siswa (siswa_id, tarif_pembayaran_id, tanggal_tagihan, jumlah_tagihan) 
                   VALUES (:siswa_id, :tarif_pembayaran_id, :tanggal_tagihan, :jumlah_tagihan)";
     $stmt = $db->prepare($insertSql);
 
-    // Iterasi melalui setiap tagihan dan lakukan insert
     foreach ($tagihanData as $data) {
-        // Bind parameters
+        // Cari tarif_pembayaran_id berdasarkan jenis pembayaran
+        $tarifStmt->bindParam(':tipe', $_POST['tarif_pembayaran_tipe']);
+        $tarifStmt->bindParam(':tahun_ajaran_id', $_POST['tahun_ajaran_id']);
+        $tarifStmt->bindParam(':jenis_pembayaran_id', $data['jenis_pembayaran_id']);
+        $tarifStmt->execute();
+
+        $tarifResult = $tarifStmt->fetch(PDO::FETCH_ASSOC);
+        if (!$tarifResult) {
+            $_SESSION['hasil'] = false;
+            $_SESSION['pesan'] = "Tarif pembayaran tidak ditemukan untuk jenis pembayaran ID: " . $data['jenis_pembayaran_id'];
+            echo "<meta http-equiv='refresh' content='0;url=?page=tagihan-siswa'>";
+            exit();
+        }
+
+        $tarif_pembayaran_id = $tarifResult['tarif_pembayaran_id'];
+
+        // Bind parameter untuk insert
         $stmt->bindParam(':siswa_id', $_POST['siswa_id']);
-        $stmt->bindParam(':tarif_pembayaran_id', $data['tarif_pembayaran_id']);
+        $stmt->bindParam(':tarif_pembayaran_id', $tarif_pembayaran_id);
         $stmt->bindParam(':tanggal_tagihan', $_POST['tanggal_tagihan']);
         $stmt->bindParam(':jumlah_tagihan', $data['jumlah_tagihan']);
 
-        // Execute the statement and check if successful
         if (!$stmt->execute()) {
             $_SESSION['hasil'] = false;
             $_SESSION['pesan'] = "Gagal simpan data";
@@ -38,8 +60,8 @@ if (isset($_POST['button_create'])) {
     echo "<meta http-equiv='refresh' content='0;url=?page=tagihan-siswa'>";
     exit();
 }
-
 ?>
+
 <form method="POST" id="form-tagihan">
     <div class="form-group">
         <label for="siswa_id">Nama</label>
@@ -59,30 +81,34 @@ if (isset($_POST['button_create'])) {
             ?>
         </select>
 
-        <label for="tarif_pembayaran_id">Jenis Pembayaran</label>
-        <select name="tarif_pembayaran_id" id="tarif_pembayaran_id" class="form-select">
+        <label for="tarif_pembayaran_tipe">Tipe Pembayaran</label>
+        <select name="tarif_pembayaran_tipe" id="tarif_pembayaran_tipe" class="form-select">
             <option value="">- Pilih -</option>
             <?php 
             $selectTarifSQL = "
                 SELECT 
                     tp.tipe, 
+                    ta.id AS tahun_ajaran_id, 
                     ta.tahun_ajaran, 
                     j.jenjang, 
                     GROUP_CONCAT(CONCAT_WS(':', tp.jenis_pembayaran_id, tp.nominal)) AS pembayaran 
                 FROM tarif_pembayaran tp
                 JOIN tahun_ajaran ta ON tp.tahun_ajaran_id = ta.id
                 JOIN jenjang j ON tp.jenjang_id = j.id
-                GROUP BY tp.tipe, ta.tahun_ajaran, j.jenjang ORDER BY ta.tahun_ajaran, j.jenjang ASC";
+                GROUP BY tp.tipe, ta.id, ta.tahun_ajaran, j.jenjang 
+                ORDER BY ta.tahun_ajaran, j.jenjang ASC";
             $stmtTarif = $db->prepare($selectTarifSQL);
             $stmtTarif->execute();
 
             while ($rowTarif = $stmtTarif->fetch(PDO::FETCH_ASSOC)){
-                echo "<option value='{$rowTarif['tipe']}' data-pembayaran='{$rowTarif['pembayaran']}'>
+                echo "<option value='{$rowTarif['tipe']}' data-tahun-ajaran='{$rowTarif['tahun_ajaran_id']}' data-pembayaran='{$rowTarif['pembayaran']}'>
                         Tipe {$rowTarif['tipe']} | Tahun Ajaran {$rowTarif['tahun_ajaran']} | {$rowTarif['jenjang']}
                       </option>";
             }
             ?>
         </select>
+
+        <input type="hidden" name="tahun_ajaran_id" id="tahun_ajaran_id">
 
         <label for="tanggal_tagihan">Tanggal Tagihan</label>
         <input type="date" name="tanggal_tagihan" class="form-control">
@@ -104,8 +130,11 @@ if (isset($_POST['button_create'])) {
 </form>
 
 <script>
-document.getElementById('tarif_pembayaran_id').addEventListener('change', function() {
+document.getElementById('tarif_pembayaran_tipe').addEventListener('change', function() {
     var pembayaran = this.options[this.selectedIndex].getAttribute('data-pembayaran');
+    var tahunAjaranId = this.options[this.selectedIndex].getAttribute('data-tahun-ajaran');
+    document.getElementById('tahun_ajaran_id').value = tahunAjaranId;
+
     var pembayaranArray = pembayaran.split(',');
 
     // Reset all fields before setting new values
