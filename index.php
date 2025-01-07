223a746b3f9f52f582651eb2ef67b6d4b013cee1
Ahmad Syarwani <ala52798@gmail.com>
2025-01-07 10:30:16 +0800

    pembuatan print

--- index.php
+++ index.php
@@ -87,12 +87,12 @@ $totalTransaksiKasir = $countTransaksi->fetch(PDO::FETCH_ASSOC)['transaksi_kasir
         <!--begin::App Main-->
         <main class="app-main"> 
             <!--begin::App Content Header-->
-            <div class="app-content-header"> 
+            <div class="app-content-header no-print"> 
                 <!--begin::Container-->
                 <div class="container-fluid"> 
                     <!--begin::Row-->
                     <div class="row">
-                        <div class="col-sm-6 no-print">
+                        <div class="col-sm-6">
                             <h3 class="mb-0">Dashboard</h3>
                         </div>
                         <div class="col-sm-6">
