10844fbf6df57a219420ae04e160d94eb5731863
Ahmad Syarwani <ala52798@gmail.com>
2025-02-19 10:11:10 +0800

    Cetak laporan transaksi kasir

--- pages/kasir/transaksi/printAll.php
+++ pages/kasir/transaksi/printAll.php
@@ -88,7 +88,7 @@
     <div class="header-line"></div>
     <div class="header-line-bottom"></div>
     <div class="content">
-        <h1 align="center">LAPORAN TRANSAKSI KASIR</h1>
+        <h3 align="center">LAPORAN TRANSAKSI KASIR</h3>
         <table class="table table-bordered">
             <thead class="table">
                 <tr>
