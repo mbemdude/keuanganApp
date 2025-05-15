2441a76e6293fbb0120efa0243a927d34d92eb86
Ahmad Syarwani <ala52798@gmail.com>
2025-05-15 10:04:33 +0800

    update routes hapus semua nilai alternatif

--- pages/admin/nilaiAlternatif/read.php
+++ pages/admin/nilaiAlternatif/read.php
@@ -30,7 +30,7 @@
             <div class="card">
               <div class="card-header">
                 <a href="?page=generate-nilai-alternatif" class="btn btn-success">Generate Data <i class="bi bi-plus-circle-fill"></i></a>
-                <a href="#" onclick="confirmDelete('?page=hapus-semua-nliai-alternatif')" class="btn btn-danger">Hapus Semua Data <i class="bi bi-trash-fill"></i></a>
+                <a href="#" onclick="confirmDelete('?page=hapus-semua-nilai-alternatif')" class="btn btn-danger">Hapus Semua Data <i class="bi bi-trash-fill"></i></a>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
--- routes.php
+++ routes.php
@@ -109,7 +109,7 @@ $routes = [
     'nilai-alternatif' => 'pages/admin/nilaiAlternatif/read.php',
     'generate-nilai-alternatif' => 'pages/admin/nilaiAlternatif/add.php',
     'hapus-nliai-alternatif' => 'pages/admin/nilaiAlternatif/delete.php',
-    'hapus-semua-nliai-alternatif' => 'pages/admin/nilaiAlternatif/deleteAll.php',
+    'hapus-semua-nilai-alternatif' => 'pages/admin/nilaiAlternatif/deleteAll.php',
 
     // Operator Section
     'mata-pelajaran' => 'pages/masterdata/mataPelajaran/read.php',
