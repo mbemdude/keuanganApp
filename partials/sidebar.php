a935dc2417f20a4b6e48fc4474f237f66567e436
Ahmad Syarwani <ala52798@gmail.com>
2025-05-17 18:05:28 +0800

    penambahan menu perhitungan spk

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -266,7 +266,7 @@
                                     </a> 
                                 </li>
                                 <li class="nav-item"> 
-                                    <a href="#" class="nav-link"> 
+                                    <a href="?page=perhitungan-spk" class="nav-link"> 
                                         <i class="nav-icon bi bi-clipboard2-pulse-fill"></i>
                                         <p>Perhitungan SPK</p>
                                     </a> 
