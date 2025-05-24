366b498aa61ecf78c76783243a7b74d65a858d17
Ahmad Syarwani <ala52798@gmail.com>
2025-05-24 08:03:18 +0800

    perubahan header impor dan ekspor nilai uts dan presensi

--- pages/operator/nilaiRapor/uts/export.php
+++ pages/operator/nilaiRapor/uts/export.php
@@ -164,7 +164,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
         <div class="col-lg-6 col-sm-12">
             <div class="card mx-3">
                 <div class="card-header">
-                    <h3 class="card-title">Ekspor/Impor Data Siswa</h3>
+                    <h3 class="card-title">Ekspor/Impor Data</h3>
                 </div>
                 <div class="card-body">
                     <form action="" method="post" class="mb-4">
@@ -189,7 +189,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
         <div class="col-lg-6 col-sm-12">
             <div class="card mx-3">
                 <div class="card-header">
-                    <h3 class="card-title">Tata Cara Import Presensi</h3>
+                    <h3 class="card-title">Cara Import</h3>
                 </div>
                 <div class="card-body">
                     <ul>
