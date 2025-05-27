3490ebd7a26a8f96edd74ce9453c0673be372367
Ahmad Syarwani <ala52798@gmail.com>
2025-05-27 12:16:10 +0800

    update icon sidebar

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,21 +1,16 @@
-        <aside class="app-sidebar bg-body-secondary shadow no-print" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
-            <div class="sidebar-brand"> <!--begin::Brand Link--> 
-                <a href="?page=home" class="brand-link"> 
-                    <!--begin::Brand Image--> 
+        <aside class="app-sidebar bg-body-secondary shadow no-print" data-bs-theme="dark">
+            <div class="sidebar-brand">
+                <a href="?page=home" class="brand-link">
                     <img src="assets/image/logoats3.png" alt="AdminLTE Logo" class="brand-image opacity-75 shadow"> 
-                    <!--end::Brand Image--> 
-                    <!--begin::Brand Text--> 
                     <span class="brand-text fw-light">Yayasan Sa'adah</span>
-                    <!--end::Brand Text--> 
-                </a> <!--end::Brand Link-->
-            </div> <!--end::Sidebar Brand--> 
-            <!--begin::Sidebar Wrapper-->
+                </a>
+            </div>
             <div class="sidebar-wrapper">
-                <nav class="mt-2"> <!--begin::Sidebar Menu-->
+                <nav class="mt-2">
                     <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
                         <li class="nav-item"> 
                             <a href="?page=home" class="nav-link active"> 
-                                <i class="nav-icon bi bi-speedometer"></i>
+                                <i class="nav-icon bi bi-house-fill"></i>
                                 <p>Dashboard</p>
                             </a>
                         </li>
@@ -39,25 +34,25 @@
                             <ul class="nav nav-treeview">
                                 <li class="nav-item"> 
                                     <a href="?page=uang-saku" class="nav-link"> 
-                                        <i class="nav-icon bi bi-wallet2"></i>
+                                        <i class="nav-icon bi bi-wallet-fill"></i>
                                         <p>Uang Saku</p>
                                     </a> 
                                 </li>
                                 <li class="nav-item"> 
                                     <a href="?page=tarif-pembayaran" class="nav-link"> 
-                                        <i class="nav-icon bi bi-receipt-cutoff"></i>
+                                        <i class="nav-icon bi bi-receipt"></i>
                                         <p>Tarif Pembayaran</p>
                                     </a> 
                                 </li>
                                 <li class="nav-item"> 
                                     <a href="?page=tagihan-siswa" class="nav-link"> 
-                                        <i class="nav-icon bi bi-cash"></i>
+                                        <i class="nav-icon bi bi-cash-coin"></i>
                                         <p>Tagihan Siswa</p>
                                     </a> 
                                 </li>
                                 <li class="nav-item"> 
                                     <a href="?page=transaksi-keuangan" class="nav-link"> 
-                                        <i class="nav-icon bi bi-credit-card-fill"></i>
+                                        <i class="nav-icon bi bi-credit-card-2-back-fill"></i>
                                         <p>Transaksi Keuangan</p>
                                     </a> 
                                 </li>
@@ -77,19 +72,19 @@
                             <ul class="nav nav-treeview">
                                 <li class="nav-item"> 
                                     <a href="?page=supplier" class="nav-link"> 
-                                        <i class="nav-icon bi bi-person-fill"></i>
+                                        <i class="nav-icon bi bi-truck"></i>
                                         <p>Supplier</p>
                                     </a> 
                                     <a href="?page=barang-masuk" class="nav-link"> 
-                                        <i class="nav-icon bi bi-box2-fill"></i>
+                                        <i class="nav-icon bi bi-box-arrow-in-down"></i>
                                         <p>Barang Masuk</p>
                                     </a> 
                                     <a href="?page=barang" class="nav-link"> 
-                                        <i class="nav-icon bi bi-box-seam-fill"></i>
+                                        <i class="nav-icon bi bi-box-seam"></i>
                                         <p>Stock Barang</p>
                                     </a> 
                                     <a href="?page=transaksi" class="nav-link"> 
-                                        <i class="nav-icon bi bi-receipt-cutoff"></i>
+                                        <i class="nav-icon bi bi-receipt"></i>
                                         <p>Transaksi</p>
                                     </a> 
                                 </li>
@@ -109,13 +104,13 @@
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
                                     <a href="?page=presensi" class="nav-link">
-                                        <i class="nav-icon bi bi-person-check-fill"></i>
+                                        <i class="nav-icon bi bi-calendar-check"></i>
                                         <p>Presensi Siswa</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
                                     <a href="#" class="nav-link">
-                                        <i class="nav-icon bi bi-clipboard2-data-fill"></i>
+                                        <i class="nav-icon bi bi-clipboard-data"></i>
                                         <p>
                                             Nilai Rapor
                                             <i class="nav-arrow bi bi-chevron-right"></i>
@@ -124,7 +119,7 @@
                                     <ul class="nav nav-treeview">
                                         <li class="nav-item">
                                             <a href="?page=nilai-rapor-uts" class="nav-link">
-                                                <i class="nav-icon bi bi-clipboard2-data-fill"></i>
+                                                <i class="nav-icon bi bi-journal-text"></i>
                                                 <p>
                                                     UTS
                                                 </p>
@@ -132,7 +127,7 @@
                                         </li>
                                         <li class="nav-item">
                                             <a href="?page=nilai-rapor-uas" class="nav-link">
-                                                <i class="nav-icon bi bi-clipboard2-data-fill"></i>
+                                                <i class="nav-icon bi bi-journal-check"></i>
                                                 <p>
                                                     UAS
                                                 </p>
@@ -161,31 +156,31 @@
                             <ul class="nav nav-treeview">
                                 <li class="nav-item"> 
                                     <a href="?page=jenjang" class="nav-link"> 
-                                        <i class="nav-icon bi bi-file-earmark-fill"></i>
+                                        <i class="nav-icon bi bi-building"></i>
                                         <p>Jenjang</p>
                                     </a>
                                 </li>
                                 <li class="nav-item"> 
                                     <a href="?page=kelas" class="nav-link"> 
-                                        <i class="nav-icon bi bi-file-earmark-fill"></i>
+                                        <i class="nav-icon bi bi-door-open"></i>
                                         <p>Kelas</p>
                                     </a>
                                 </li>
                                 <li class="nav-item"> 
                                     <a href="?page=status" class="nav-link"> 
-                                        <i class="nav-icon bi bi-file-earmark-fill"></i>
+                                        <i class="nav-icon bi bi-check-circle-fill"></i>
                                         <p>Status</p>
                                     </a>
                                 </li>
                                 <li class="nav-item"> 
                                     <a href="?page=tahun-ajaran" class="nav-link"> 
-                                        <i class="nav-icon bi bi-file-earmark-fill"></i>
+                                        <i class="nav-icon bi bi-calendar-range-fill"></i>
                                         <p>Tahun Ajaran</p>
                                     </a>
                                 </li>
                                 <li class="nav-item"> 
                                     <a href="?page=mata-pelajaran" class="nav-link"> 
-                                        <i class="nav-icon bi bi-file-earmark-fill"></i>
+                                        <i class="nav-icon bi bi-book-half"></i>
                                         <p>Mata Pelajaran</p>
                                     </a>
                                 </li>
@@ -204,7 +199,7 @@
                             <ul class="nav nav-treeview">
                                 <li class="nav-item"> 
                                     <a href="?page=jenis-pembayaran" class="nav-link"> 
-                                        <i class="nav-icon bi bi-file-earmark-fill"></i>
+                                        <i class="nav-icon bi bi-tags-fill"></i>
                                         <p>Jenis Pembayaran</p>
                                     </a>
                                 </li>
@@ -217,7 +212,7 @@
                         </li>
                         <li class="nav-item"> 
                             <a href="#" class="nav-link"> 
-                                <i class="nav-icon bi bi-person-badge-fill"></i>
+                                <i class="nav-icon bi bi-shield-lock-fill"></i>
                                 <p>
                                     Pengaturan User
                                     <i class="nav-arrow bi bi-chevron-right"></i>
@@ -226,13 +221,13 @@
                             <ul class="nav nav-treeview">
                                 <li class="nav-item"> 
                                     <a href="?page=role" class="nav-link"> 
-                                        <i class="nav-icon bi bi-person-fill-gear"></i>
+                                        <i class="nav-icon bi bi-person-badge"></i>
                                         <p>Role</p>
                                     </a> 
                                 </li>
                                 <li class="nav-item"> 
                                     <a href="?page=user" class="nav-link"> 
-                                        <i class="nav-icon bi bi-person-plus-fill"></i>
+                                        <i class="nav-icon bi bi-person-plus"></i>
                                         <p>User</p>
                                     </a> 
                                 </li>
@@ -249,13 +244,13 @@
                             <ul class="nav nav-treeview">
                                 <li class="nav-item"> 
                                     <a href="?page=kriteria" class="nav-link"> 
-                                        <i class="nav-icon bi bi-clipboard2-check-fill"></i>
+                                        <i class="nav-icon bi bi-clipboard2-pulse"></i>
                                         <p>Kriteria</p>
                                     </a> 
                                 </li>
                                 <li class="nav-item"> 
                                     <a href="?page=nilai-alternatif" class="nav-link"> 
-                                        <i class="nav-icon bi bi-clipboard2-check-fill"></i>
+                                        <i class="nav-icon bi bi-card-checklist"></i>
                                         <p>Nilai Alternatif</p>
                                     </a> 
                                 </li>
@@ -270,13 +265,13 @@
                                     <ul class="nav nav-treeview">
                                         <li class="nav-item"> 
                                             <a href="?page=normalisasi" class="nav-link"> 
-                                                <i class="nav-icon bi bi-clipboard2-data-fill"></i>
+                                                <i class="nav-icon bi bi-filter-square-fill"></i>
                                                 <p>Normalisasi SAW</p>
                                             </a> 
                                         </li>
                                         <li class="nav-item"> 
-                                            <a href="?page=perhitungan-spk" class="nav-link"> 
-                                                <i class="nav-icon bi bi-clipboard2-pulse-fill"></i>
+                                            <a href="?page=perhitungan-saw" class="nav-link"> 
+                                                <i class="nav-icon bi bi-bar-chart-line-fill"></i>
                                                 <p>Perhitungan SAW</p>
                                             </a> 
                                         </li>
@@ -284,7 +279,7 @@
                                 </li>
                                 <li class="nav-item">
                                     <a href="#" class="nav-link">
-                                        <i class="nav-icon bi bi-clipboard2-check-fill"></i>
+                                        <i class="nav-icon bi bi-diagram-3"></i>
                                         <p>
                                             AHP
                                             <i class="nav-arrow bi bi-chevron-right"></i>
@@ -293,25 +288,25 @@
                                     <ul class="nav nav-treeview">
                                         <li class="nav-item"> 
                                             <a href="?page=perbandingan-kriteria" class="nav-link"> 
-                                                <i class="nav-icon bi bi-clipboard2-check-fill"></i>
+                                                <i class="nav-icon bi bi-list-columns-reverse"></i>
                                                 <p>Perbandingan Kriteria</p>
                                             </a> 
                                         </li>
                                         <li class="nav-item"> 
                                             <a href="?page=bobot-kriteria" class="nav-link"> 
-                                                <i class="nav-icon bi bi-clipboard2-check-fill"></i>
+                                                <i class="nav-icon bi bi-sliders2"></i>
                                                 <p>Bobot Kriteria</p>
                                             </a> 
                                         </li>
                                         <li class="nav-item"> 
                                             <a href="?page=normalisasi-ahp" class="nav-link"> 
-                                                <i class="nav-icon bi bi-clipboard2-check-fill"></i>
+                                                <i class="nav-icon bi bi-filter-circle"></i>
                                                 <p>Normalisasi AHP</p>
                                             </a> 
                                         </li>
                                         <li class="nav-item"> 
                                             <a href="?page=perhitungan-ahp" class="nav-link"> 
-                                                <i class="nav-icon bi bi-clipboard2-check-fill"></i>
+                                                <i class="nav-icon bi bi-award-fill"></i>
                                                 <p>Perhitungan AHP</p>
                                             </a> 
                                         </li>
@@ -321,8 +316,6 @@
                         </li>
                         <?php endif; ?>
                     </ul> 
-                    <!--end::Sidebar Menu-->
                 </nav>
             </div> 
-            <!--end::Sidebar Wrapper-->
         </aside> 
